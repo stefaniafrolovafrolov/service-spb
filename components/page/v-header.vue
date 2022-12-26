e4f877377dcb6a08493af0d19d9dcd0d6d5373b9
Vadim Frolov <stefaniafrolovafrolov@yandex.ru>
2022-12-26 21:10:16 +0300

    Начинаем работать с сайтом сервиса

---
+++ components/page/v-header.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  dddddddddddd
+</div>
+</template>
+
+<script>
+export default {
+  name: "v-header"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <v-header/>
+  <nuxt/>
+</div>
+</template>
+
+<script>
+import vHeader from "@/components/page/v-header"
+export default {
+  name: "default",
+  components: {
+    vHeader
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
