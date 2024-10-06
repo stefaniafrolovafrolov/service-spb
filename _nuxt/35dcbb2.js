b89b029ebc9ff78030fca82f42570ecf6171c4e8
Vadim Frolov <stefaniafrolovafrolov@yandex.ru>
2024-10-06 12:47:00 +0300

    96aa7f3

---
+++ _nuxt/35dcbb2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{315:function(e,t,r){"use strict";r.r(t);r(215);t.default={name:"mixin-slide",props:{item:{type:Object},itemIndex:{type:Number,required:!0},itemsLength:{type:Number,required:!0},activeSlideIndex:{type:Number,required:!0},dataInSlide:{type:Object,required:!0}}}}}]);
